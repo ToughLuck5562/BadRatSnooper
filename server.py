eade0faf20fa5660882ab0a016e6642754b1a358
ToughLuck <94546656+ToughLuck5562@users.noreply.github.com>
2025-03-14 09:24:30 -0400

    Create server.py

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__, template_folder="client/tempaltes", static_folder="client/static")
+
+@app.route("/"):
+def default():
+  return 404
+
+@app.route("/dsh")
+def dashboard():
+  pass
+
+@app.route("/cnts", methods=["POST", "GET"])
+  Device = request.args.get('ip')
+  NewData = reuqest.args.get('data')
+
+  if request.method == "POST":
+    # upload to mongodb
+    
+    return render_template("cnts.html", ip=Device)
